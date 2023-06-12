e8fa966d3fc36907c3e47cc74342608e31be44eb
C-Carroll <crcarroll00@gmail.com>
2023-06-12 18:30:29 -0500

    updated migrations

--- backend/db/migrations/20230606235602-create-review.js
+++ backend/db/migrations/20230606235602-create-review.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
+    await queryInterface.createTable(options, 'Reviews', {
       id: {
         allowNull: false,
         autoIncrement: true,
